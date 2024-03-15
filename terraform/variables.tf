8e6b324a5e318f4b2a82d0547d097579ec85d73e
mmurali12 <mutyalamurali1@gmail.com>
2024-03-15 15:16:34 +0530

    code updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "vpc-name" {
 }
 
 variable "igw-name" {
-  description = "Internet Gate Way Name for our Jumphost server"
+  description = "InternetGate Way Name for our Jumphost server"
   type = string
   default = "Jumphost-igw"
 }
